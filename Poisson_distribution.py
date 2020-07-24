0059fd56e75ba50fb71801602c450854b486d0b6
Shantanu singh <59371629+shan-2205@users.noreply.github.com>
2020-07-25 00:30:18 +0530

    Add files via upload

--- Poisson_distribution.py
+++ Poisson_distribution.py
@@ -1,7 +1,35 @@
+#Question solution on POisson
+from scipy.stats import poisson
+poisson.pmf(3,2)
+
+
+
+
+
+
+
+
+
+
 
 import numpy as np
 import pandas as pd
 import scipy.stats as stats
 
-school_ages=stats.poisson.rvs(loc=18,mu=35,size=1500)
-school_ages
+# Generating Random Numbers which follows poisson distribution path
+school_ages=stats.poisson.rvs(mu=35,size=1500)
+sns.distplot(school_ages)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
