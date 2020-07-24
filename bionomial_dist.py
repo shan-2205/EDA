ed110102f8e089996afe651e2aecc18f343f9ee3
Shantanu singh <59371629+shan-2205@users.noreply.github.com>
2020-07-24 20:01:46 +0530

    Add files via upload

--- bionomial_dist.py
+++ bionomial_dist.py
@@ -1,13 +1,33 @@
 from scipy.stats import binom
 import seaborn as sns
 
-#n is basically ur highest number in ur array
-data_binom = binom.rvs(n=30,p=0.8,size=1000)
 
 
 
-#visualising by graph
-sns.distplot(data_binom, hist=True, kde=False)
+
+#k-no of success, and n=total number of trials
+#Question solution
+from scipy.stats import binom
+s=binom.pmf(k=3,n=5,p=0.75)
+s
+
+
+
+
+sum=0
+for i in range(3,6):
+    from scipy.stats import binom
+    s=binom.pmf(k=i,n=5,p=0.75)
+    sum=sum+s
+    print(s)
+sum
+
+
+
+
+
+
+
 
 
 
