766db45cddb64f9c63a34b8ca4e6052148f82ebd
Shantanu singh <59371629+shan-2205@users.noreply.github.com>
2020-07-26 12:39:47 +0530

    Add files via upload

--- Log_normaldist.py
+++ Log_normaldist.py
@@ -3,3 +3,24 @@
 from scipy.stats import skewnorm
 x=skewnorm.rvs(6,size=10000)
 sns.distplot(x)
+
+
+from scipy.stats import expon
+import seaborn as sns
+data_expon = expon.rvs(scale=1,loc=0,size=1000)
+
+
+ax = sns.distplot(data_expon,
+                  kde=True,
+                  bins=100,
+                  color='skyblue',
+                  hist_kws={"linewidth": 15,'alpha':1})
+ax.set(xlabel='Exponential Distribution', ylabel='Frequency')
+
+
+
+
+
+
+
+
